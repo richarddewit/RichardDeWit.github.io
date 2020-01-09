8e36ee06af5c09a05f9a428fe3137ca54af574f9
Richard de Wit <henk.exe@gmail.com>
2020-01-09 16:19:25 +0100

    Extract components from html

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script>
-  // export let name;
+ import Footer from "@/Footer.svelte";
 </script>
 
 <style>
-  h1 {
-    color: black;
-  }
+ main {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+   position: relative;
+   z-index: 10;
+   min-height: calc(100vh - 8rem);
+   padding: 1rem;
+   font-size: 1.6rem;
+   text-shadow: #e3d5bacc 1px 1px 5px;
+ }
 </style>
 
 <h1>&nbsp;&nbsp;Soon&trade;</h1>
 <p>Under Construction</p>
+
+<Footer/>
---
+++ src/Footer.svelte
@@ -0,0 +1,75 @@
+<script>
+ let year = new Date().getFullYear();
+ let links = [
+   {
+     type: "github",
+     url: "https://github.com/richarddewit",
+     title: "Github - richarddewit",
+   },
+   {
+     type: "twitter",
+     url: "https://twitter.com/sneetzle",
+     title: "Twitter - @Sneetzle",
+   },
+   {
+     type: "stack-overflow",
+     url: "https://stackoverflow.com/users/1062281/richard-de-wit",
+     title: "Stack Overflow - Richard de Wit",
+   },
+ ];
+</script>
+
+<style>
+ footer {
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   flex-direction: column;
+   position: absolute;
+   right: 0;
+   bottom: 4rem;
+   left: 0;
+   padding: .5rem 1rem;
+   height: 4rem;
+   text-align: center;
+ }
+
+ .list-inline {
+   margin: 0;
+   padding: 0;
+   list-style: none;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+ }
+
+ .icon-link {
+   color: #e3d5ba;
+   display: block;
+   margin: 0 0.5em;
+ }
+
+ .icon-link:hover {
+   color: #e86701;
+ }
+</style>
+
+<footer>
+  <section>&copy; {year} Richard de Wit</section>
+  <section>
+    <ul class="list-inline">
+      {#each links as link}
+      <li>
+        <a
+          href="{link.url}"
+          target="_blank"
+          class="icon-link"
+          title="{link.title}"
+        >
+          <i class="fa fa-fw fa-{link.type}"></i>
+        </a>
+      </li>
+      {/each}
+    </ul>
+  </section>
+</footer>
