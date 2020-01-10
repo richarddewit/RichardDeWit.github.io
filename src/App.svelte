604c00143f974c21f8d37e0884fc93d75a2669f5
Richard de Wit <henk.exe@gmail.com>
2020-01-10 09:06:29 +0100

    Add more styling and make responsive

--- src/App.svelte
+++ src/App.svelte
@@ -13,8 +13,17 @@
    z-index: 10;
    min-height: calc(100vh - 8rem);
    padding: 1rem;
-   font-size: 1.6rem;
+   font-size: 1.6em;
    text-shadow: #e3d5bacc 1px 1px 5px;
+   max-width: 640px;
+   margin: 0 auto;
+ }
+
+ @media screen and (max-width: 400px) {
+   main {
+     font-size: 1.4em;
+     max-width: 100%;
+   }
  }
 </style>
 
